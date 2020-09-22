6e0f3eb2d3e118e4f57fd9e8f529152b294f75f1
Maryna <m.ohorodnik.22@gmail.com>
2020-09-22 09:20:32 +0300

    Add project files.

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Привіт, ДонНУ!");
+        }
+    }
+}
